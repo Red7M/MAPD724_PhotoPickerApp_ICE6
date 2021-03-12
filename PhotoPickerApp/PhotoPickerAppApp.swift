ed6568938e5851ea2bc04943274b1cb96464b376
Red7M <reda.mehali@gmail.com>
2021-03-12 15:52:26 -0500

    Initial Commit

---
+++ PhotoPickerApp/PhotoPickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerAppApp.swift
+//  PhotoPickerApp
+//
+//  Created by Reda Mehali on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
